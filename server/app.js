a5a9df82252aa3fc5f5e9ac6667d27ac92db60c1
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-08-16 08:12:59 +0900

    Setup basic backend with MongoDB connection

---
+++ server/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Basic route
+app.get('/', (req, res) => res.send('Twix API'));
+
+module.exports = app;
