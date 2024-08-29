92649725c96ee0740f3d249d145872a652b93540
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-08-29 16:13:29 +0900

    Google Tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import '../styles/globals.css';
-import Layout from '../components/Layout'; // Adjust the path if necessary
+import Layout from '../components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     return (
         <Layout>
+            <Head>
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}`}></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}');
+                    `,
+                    }}
+                />
+            </Head>
             <Component {...pageProps} />
         </Layout>
     );
